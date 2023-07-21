e5457e257c448b484de9daffd5f934ae69422900
ZExUZzzz <60745798+ZExUZzzz@users.noreply.github.com>
2023-07-21 16:16:25 +0500

    Create patcher.sh

---
+++ patcher.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+#Get list of ingress-controller pods
+kubectl get pods -n ingress-nginx | grep Running | grep ingress-nginx-controller | awk '{print $1}' > /root/controller-pods.list && cat /root/controller-pods.list
+
+#Get IP of nodes that contains this pods
+for pod in $(cat /root/controller-pods.list); do kubectl -n ingress-nginx get pod $pod --template '{{.status.hostIP}}'; echo ""; done > /root/controller-ip.list && cat /root/controller-ip.list
+
+#Collecting a variable
+ingress_ips=($(sed ':a;N;$!ba;s/\n/","/g; s/^/"/; s/$/"/' /root/controller-ip.list))
+
+#Patch ingress-controller service
+kubectl patch service ingress-nginx -n ingress-nginx -p '{"spec": {"externalIPs": ['$ingress_ips']}}'
